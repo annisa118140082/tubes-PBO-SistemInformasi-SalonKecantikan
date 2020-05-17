5b8c19ea673f3646de6e0d2f90f482bd9c8b14fd
annisadwiatika <ichaaa2000@gmail.com>
2020-05-17 15:45:30 +0700

    salon kecantikan vinanazlaicha

--- src/awal/Pelanggan.java
+++ src/awal/Pelanggan.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pbo;
+package awal;
 import java.awt.HeadlessException;
 import java.sql.Connection;
 import javax.swing.table.DefaultTableModel;
